7915559d6ed79c4db404ecee2cd671ed5e7bd03e
Rimsky Rojas <rrojas@cern.ch>
2026-01-01 01:42:32 +0100

    add also the tarball used and the top vhd for reference

---
+++ hdl/Mars_XU3_ST3.vhd
@@ -0,0 +1,258 @@
+----------------------------------------------------------------------------------------------------
+-- Copyright (c) 2025 by Enclustra GmbH, Switzerland.
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of
+-- this hardware, software, firmware, and associated documentation files (the
+-- "Product"), to deal in the Product without restriction, including without
+-- limitation the rights to use, copy, modify, merge, publish, distribute,
+-- sublicense, and/or sell copies of the Product, and to permit persons to whom the
+-- Product is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Product.
+--
+-- THE PRODUCT IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+-- INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+-- PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+-- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+-- OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+-- PRODUCT OR THE USE OR OTHER DEALINGS IN THE PRODUCT.
+----------------------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------------------
+-- libraries
+----------------------------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+----------------------------------------------------------------------------------------------------
+-- entity declaration
+----------------------------------------------------------------------------------------------------
+entity Mars_XU3_ST3 is
+  
+  port (
+    
+    -- Anios IO Connector 0
+    IO0_D0_P                       : inout   std_logic;
+    IO0_D1_N                       : inout   std_logic;
+    IO0_D2_P                       : inout   std_logic;
+    IO0_D3_N                       : inout   std_logic;
+    IO0_D4_P                       : inout   std_logic;
+    IO0_D5_N                       : inout   std_logic;
+    IO0_D6_P                       : inout   std_logic;
+    IO0_D7_N                       : inout   std_logic;
+    IO0_D8_P                       : inout   std_logic;
+    IO0_D9_N                       : inout   std_logic;
+    IO0_CLK_N                      : inout   std_logic;
+    IO0_CLK_P                      : inout   std_logic;
+    IO0_D10_P                      : inout   std_logic;
+    IO0_D11_N                      : inout   std_logic;
+    IO0_D12_P                      : inout   std_logic;
+    IO0_D13_N                      : inout   std_logic;
+    IO0_D14_P                      : inout   std_logic;
+    IO0_D15_N                      : inout   std_logic;
+    IO0_D16_P                      : inout   std_logic;
+    IO0_D17_N                      : inout   std_logic;
+    IO0_D18_P                      : inout   std_logic;
+    IO0_D19_N                      : inout   std_logic;
+    IO0_D20_P                      : inout   std_logic;
+    IO0_D21_N                      : inout   std_logic;
+    IO0_D22_MIPI_D0LP_P            : inout   std_logic;
+    IO0_D23_MIPI_D0LP_N            : inout   std_logic;
+    
+    -- Anios IO Connector 1
+    IO1_D0_P                       : inout   std_logic;
+    IO1_D1_N                       : inout   std_logic;
+    IO1_D2_P                       : inout   std_logic;
+    IO1_D3_N                       : inout   std_logic;
+    IO1_D4_P                       : inout   std_logic;
+    IO1_D5_N                       : inout   std_logic;
+    IO1_D6_P                       : inout   std_logic;
+    IO1_D7_N                       : inout   std_logic;
+    IO1_D8_P                       : inout   std_logic;
+    IO1_D9_N                       : inout   std_logic;
+    IO1_D10_P                      : inout   std_logic;
+    IO1_D11_N                      : inout   std_logic;
+    IO1_D12_P                      : inout   std_logic;
+    IO1_D13_N                      : inout   std_logic;
+    IO1_D14_P                      : inout   std_logic;
+    IO1_D15_N                      : inout   std_logic;
+    
+    -- Display Port
+    DP_HPD                         : in      std_logic;
+    DP_AUX_IN                      : in      std_logic;
+    DP_AUX_OE                      : out     std_logic;
+    DP_AUX_OUT                     : out     std_logic;
+    
+    -- HDMI
+    HDMI_CEC_WDI                   : inout   std_logic;
+    HDMI_HPD                       : in      std_logic;
+    HDMI_D0_N                      : inout   std_logic;
+    HDMI_D0_P                      : inout   std_logic;
+    HDMI_D1_N                      : inout   std_logic;
+    HDMI_D1_P                      : inout   std_logic;
+    HDMI_D2_N                      : inout   std_logic;
+    HDMI_D2_P                      : inout   std_logic;
+    HDMI_CLK_N                     : inout   std_logic;
+    HDMI_CLK_P                     : inout   std_logic;
+    
+    -- I2C PL
+    I2C_MGMT_SCL                   : inout   std_logic;
+    I2C_MGMT_SDA                   : inout   std_logic;
+    
+    -- ST3 I2C USER
+    I2C_USER_SCL                   : inout   std_logic;
+    I2C_USER_SDA                   : inout   std_logic;
+    
+    -- IO Connector 2
+    IO2_D0_P                       : inout   std_logic;
+    IO2_D1_N                       : inout   std_logic;
+    IO2_D2_P                       : inout   std_logic;
+    IO2_D3_N                       : inout   std_logic;
+    IO2_D4_P                       : inout   std_logic;
+    IO2_D5_N                       : inout   std_logic;
+    IO2_D6_P                       : inout   std_logic;
+    IO2_D7_N                       : inout   std_logic;
+    
+    -- IO Connector 3
+    IO3_D0_P                       : inout   std_logic;
+    IO3_D1_N                       : inout   std_logic;
+    IO3_D2_P                       : inout   std_logic;
+    IO3_D3_N                       : inout   std_logic;
+    
+    -- LED
+    LED0_N                         : out     std_logic;
+    LED1_N                         : out     std_logic;
+    
+    -- MIPI
+    MIPI_D0_N                      : in      std_logic;
+    MIPI_D0_P                      : in      std_logic;
+    MIPI_D1_N                      : in      std_logic;
+    MIPI_D1_P                      : in      std_logic;
+    MIPI_CLK_N                     : in      std_logic;
+    MIPI_CLK_P                     : in      std_logic
+  );
+end Mars_XU3_ST3;
+
+architecture rtl of Mars_XU3_ST3 is
+
+  ----------------------------------------------------------------------------------------------------
+  -- component declarations
+  ----------------------------------------------------------------------------------------------------
+  component Mars_XU3 is
+    port (
+      DP_AUX_OUT          : out    std_logic;
+      DP_AUX_OE           : out    std_logic;
+      DP_AUX_IN           : in     std_logic;
+      DP_HPD              : in     std_logic;
+      Clk100              : out    std_logic;
+      Clk50               : out    std_logic;
+      Rst_N               : out    std_logic;
+      IIC_USER_sda_i      : in     std_logic;
+      IIC_USER_sda_o      : out    std_logic;
+      IIC_USER_sda_t      : out    std_logic;
+      IIC_USER_scl_i      : in     std_logic;
+      IIC_USER_scl_o      : out    std_logic;
+      IIC_USER_scl_t      : out    std_logic;
+      LED1_N              : out    std_logic;
+      reg0_out_0 : out STD_LOGIC_VECTOR ( 31 downto 0 )
+    );
+  end component Mars_XU3;
+  
+
+  ----------------------------------------------------------------------------------------------------
+  -- signal declarations
+  ----------------------------------------------------------------------------------------------------
+  signal Clk100           : std_logic;
+  signal Clk50            : std_logic;
+  signal Rst_N            : std_logic;
+  signal IIC_USER_sda_i   : std_logic;
+  signal IIC_USER_sda_o   : std_logic;
+  signal IIC_USER_sda_t   : std_logic;
+  signal IIC_USER_scl_i   : std_logic;
+  signal IIC_USER_scl_o   : std_logic;
+  signal IIC_USER_scl_t   : std_logic;
+  signal dp_aux_data_oe_n : std_logic;
+  signal LedCount         : unsigned(23 downto 0);
+  signal reg0 :  STD_LOGIC_VECTOR ( 31 downto 0 );
+  
+  ----------------------------------------------------------------------------------------------------
+  -- attribute declarations
+  ----------------------------------------------------------------------------------------------------
+
+begin
+  
+  ----------------------------------------------------------------------------------------------------
+  -- processor system instance
+  ----------------------------------------------------------------------------------------------------
+  Mars_XU3_i: component Mars_XU3
+    port map (
+      DP_AUX_OUT           => DP_AUX_OUT,
+      DP_AUX_OE            => dp_aux_data_oe_n,
+      DP_AUX_IN            => DP_AUX_IN,
+      DP_HPD               => DP_HPD,
+      Clk100               => Clk100,
+      Clk50                => Clk50,
+      Rst_N                => Rst_N,
+      IIC_USER_sda_i       => IIC_USER_sda_i,
+      IIC_USER_sda_o       => IIC_USER_sda_o,
+      IIC_USER_sda_t       => IIC_USER_sda_t,
+      IIC_USER_scl_i       => IIC_USER_scl_i,
+      IIC_USER_scl_o       => IIC_USER_scl_o,
+      IIC_USER_scl_t       => IIC_USER_scl_t,
+      LED1_N               => LED1_N,
+      reg0_out_0           => reg0
+    );
+  
+  DP_AUX_OE <= not dp_aux_data_oe_n;
+  
+  hdmi_clock_buf: component OBUFDS
+    port map (
+      I => '0',
+      O => HDMI_CLK_P,
+      OB => HDMI_CLK_N
+    );
+  
+  hdmi_d0_buf: component OBUFDS
+    port map (
+      I => '0',
+      O => HDMI_D0_P,
+      OB => HDMI_D0_N
+    );
+  
+  hdmi_d1_buf: component OBUFDS
+    port map (
+      I => '0',
+      O => HDMI_D1_P,
+      OB => HDMI_D1_N
+    );
+  
+  hdmi_d2_buf: component OBUFDS
+    port map (
+      I => '0',
+      O => HDMI_D2_P,
+      OB => HDMI_D2_N
+    );
+  I2C_USER_SDA  <= IIC_USER_sda_o when IIC_USER_sda_t = '0' else 'Z';
+  IIC_USER_sda_i <= I2C_USER_SDA;
+  I2C_USER_SCL <= IIC_USER_scl_o when IIC_USER_scl_t = '0' else 'Z';
+  IIC_USER_scl_i <= I2C_USER_SCL;
+  
+  process (Clk50)
+  begin
+    if rising_edge (Clk50) then
+      if Rst_N = '0' then
+        LedCount    <= (others => '0');
+      else
+        LedCount    <= LedCount + 1;
+      end if;
+    end if;
+  end process;
+ -- Led0_N <= '0' when LedCount(LedCount'high) = '0' else 'Z';
+  Led0_N <= reg0(0);
+  
+end rtl;
